4b2fb85846261efbe45617c556f96d7098c16590
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-08-05 21:39:54 +0530

    Fix typo in MongoDB connection string password placeholder

--- 16_python_mongodb/youtube_manager_mongodb.py
+++ 16_python_mongodb/youtube_manager_mongodb.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from bson import ObjectId
 
-client = MongoClient("mongodb+srv://<DB_NAME>:<DB_PAS >@cluster0.jnxhi15.mongodb.net/", tlsAllowInvalidCertificates=True)
+client = MongoClient("mongodb+srv://<DB_NAME>:<DB_PASSWORD>@cluster0.jnxhi15.mongodb.net/", tlsAllowInvalidCertificates=True)
 # Not a good idea to include id and password in code files
 # tlsAllowInvalidCertificates=True - Not a good way to handle ssl
 
